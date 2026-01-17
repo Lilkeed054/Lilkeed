8a695d5865e709af57a967e7a4c943d794094245
Lilkeed054 <siraritaonashe@gmail.com>
2026-01-16 20:08:56 -1200

    Fix Netlify SPA routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import SideNavigation from './components/SideNavigation';
